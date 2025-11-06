0d5fa4806ac44cc1019d1f3aafd8105db6f42cb5
Nikola <nikola.lazarov2017@gmail.com>
2025-11-06 12:04:09 +0200

    fixed

--- src/utils/vivaWallet.js
+++ src/utils/vivaWallet.js
@@ -1,23 +1,24 @@
-export async function VivaWallet_MakePay(_id_doc:number, _amount:number) {
+export async function VivaWallet_MakePay(_id_doc, _amount) {
   if (!_amount) throw "Няма сума за плащане!"; // -->
   const scheme = 'vivapayclient://pay/v1';
   const params = {
     appId: 'bg.gencloud.mobile',
     action: 'sale',
     clientTransactionId: _id_doc,    // unique ID for your tracking
-    amount: (_amount * 100).Round(), // 1000 cents = €10.00
+    amount: Math.round(_amount * 100), // 1000 cents = €10.00
     tipAmount: '0',                  // бакшиш
-    callback: Gen.IsInDebug ? 'http://192.168.10.10:4040/~/VivaWallet/ResultSet' : window.location.origin + '/VivaWallet/ResultSet',
+    callback: window.location.origin,
     show_receipt: 'false',
     show_transaction_result: 'false',
     show_rating: 'false'
   };
 
-  const query = Object.entries(params)
-    .map(([k, v]) => ${encodeURIComponent(k)}=${encodeURIComponent(v)})
-    .join('&');
+const query = Object.entries(params)
+  .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+  .join('&');
 
-  const url = ${scheme}?${query};
+
+const url = `${scheme}?${query}`;
 
   const a = document.createElement('a');
   a.href = url;
